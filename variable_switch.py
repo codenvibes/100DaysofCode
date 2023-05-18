28bfd481cad6d12a69b586730d278e95a2bb48ad
codenvibes <terrymk1999@gmail.com>
2023-05-18 12:58:17 +0300

    Create a program that switches the values of a and b

---
+++ variable_switch.py
@@ -0,0 +1,18 @@
+# Write a program that switches the values stored in the variables a and b.
+
+# 🚨 Don't change the code below 👇
+a = (input("a : "))
+b = (input("b : "))
+# 🚨 Don't change the code above 👆
+#================================================================================================================
+#Write your code below this line 👇
+# Assume you have two cups: Cup a has water in it and cup b has milk and you want to switch the contents into opposite cups. You'll need to use an extra empty container.
+c = a
+a = b
+b = c
+#================================================================================================================
+# 🚨 Don't change the code below 👇
+print()
+print("After switching;")
+print("a : ", a)
+print("b : ", b)
