40922b951fe31e3d98a1dba70ee172b55be1eade
codenvibes <terrymk1999@gmail.com>
2023-05-19 08:57:54 +0300

    Create a program that adds the digits in a 2 digit number

--- Day_2/data_types.py
+++ Day_2/data_types.py
@@ -4,7 +4,6 @@ two_digit_number = input("Type a two digit number: ")
 
 ####################################
 #Write your code below this line 👇
-two_digit_string = str(two_digit_number)
-two_digit_string_0 = (two_digit_string[0])
-two_digit_string_1 = (two_digit_string[1])
-print(int(two_digit_string_0) + int(two_digit_string_1))
+
+# print(type(two_digit_number))
+print(int(two_digit_number[0]) + int(two_digit_number[1]))
