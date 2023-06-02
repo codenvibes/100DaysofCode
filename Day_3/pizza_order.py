17b709d8ddb3b602c4fa70795cb02ff3f0bfa8ab
codenvibes <terrymk1999@gmail.com>
2023-06-02 16:15:47 +0300

    Build an automatic pizza order program

---
+++ Day_3/pizza_order.py
@@ -0,0 +1,33 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+# Pizza Size
+bill = 0
+if size == "L":
+    bill = 25
+elif size == "M":
+    bill = 20
+elif size == "S":
+    bill = 15
+
+# Add Pepperoni
+if (size == "L" or size == "M") and add_pepperoni == "Y":
+    bill += 3
+elif size == "S" and add_pepperoni == "Y":
+    bill += 2
+elif add_pepperoni == "N":
+    bill += 0
+
+# Add cheese
+if extra_cheese == "Y":
+    bill += 1
+elif extra_cheese == "N":
+    bill += 0
+
+print(f"Your final bill is: ${bill}.")
+
