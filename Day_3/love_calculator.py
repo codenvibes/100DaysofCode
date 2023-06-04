a2360e16fd8a036b44099fb1c025cc971cb360a2
codenvibes <terrymk1999@gmail.com>
2023-06-04 12:04:31 +0300

    Create a program that tests the compatibility between two people

--- Day_3/love_calculator.py
+++ Day_3/love_calculator.py
@@ -6,22 +6,25 @@ name2 = input("What is their name? \n")
 
 #Write your code below this line 👇
 
-# count TRUE
-count_T = name1.lower().count('t') + name2.lower().count('t')
-count_R = name1.lower().count('r') + name2.lower().count('r')
-count_U = name1.lower().count('u') + name2.lower().count('u')
-count_E = name1.lower().count("e") + name2.lower().count('e')
+combined_string = name1 + name2
+lower_case_string = combined_string.lower()
 
-# Count LOVE
-count_L = name1.lower().count('l') + name2.lower().count('l')
-count_O = name1.lower().count('o') + name2.lower().count('o')
-count_V = name1.lower().count('v') + name2.lower().count('v')
+# count true
+t = lower_case_string.count('t')
+r = lower_case_string.count('r')
+u = lower_case_string.count('u')
+e = lower_case_string.count('e')
+
+# count love
+l = lower_case_string.count('l')
+o = lower_case_string.count('o')
+v = lower_case_string.count('v')
 
 # Totals
-total1 = count_T + count_R + count_U + count_E
-total2 = count_L + count_O + count_V + count_E
+true = t + r + u + e
+love = l + o + v + e
 
-love_score = int(str(total1) + str(total2))
+love_score = int(str(true) + str(love))
 
 if love_score < 10 or love_score > 90:
     print(f"Your score is {love_score}, you go together like coke and mentos.")
